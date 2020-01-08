138d89f924030ed605a30fd212fbf267026dddaf
Stephen <stephenyoon84@gmail.com>
2020-01-08 14:27:44 -0500

    make glyphicon works

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-favorite',
+  selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css']
 })
